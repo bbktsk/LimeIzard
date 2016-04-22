b672e1e7ef696d84ddf5eb5de0f5ad2cc874da4b
Bob Koutsky <bohumil.koutsky@gooddata.com>
2016-04-23 00:18:44 +0200

    REST API style nazi

--- src/lime_izard/web.clj
+++ src/lime_izard/web.clj
@@ -12,7 +12,8 @@
             [clojure.java.jdbc :as db]
             [slingshot.slingshot :refer [throw+]]
             [clojure.walk :refer [keywordize-keys]]
-            [liberator.core :refer [resource defresource]]))
+            [liberator.core :refer [resource defresource]]
+            [liberator.dev :refer [wrap-trace]]))
 
 (defn db-spec
   []
@@ -20,6 +21,8 @@
 
 (def user-fields [:first_name :last_name :mood :message :photo_url :fb_id :sex
                   :active])
+
+(def visit-fields [:beacon_id :signal :longitude :latitude])
 
 (defn empty-map [fields]
   (apply hash-map (interleave fields (repeat nil))))
@@ -36,24 +39,63 @@
 
 (defn user-update
   [id data]
+  (println data)
   (if-let [current (user-get id)]
     (q-user-update! (assoc (merge current data) :fb_id id))
-    (q-user-insert! (assoc (ensure-fields data user-fields) :fb_id id))))
+    (throw+ "internal error, user vanished")))
 
+(defn user-create!
+  [data]
+  (q-user-insert! (ensure-fields data user-fields)))
+
+(defn new-user-valid?
+  [data]
+  (nil? (:fb_id data)))
+
+(defn visit-valid?
+  [visit]
+  (and (every? visit visit-fields)
+       (= (count visit-fields) (count (keys visit)))))
+
+
+(defn handle-visit
+  [id visit]
+  )
 
 (defresource r-user-get [id]
   :available-media-types ["application/json"]
   :exists? (fn [ctx] (if-let [x (user-get id)] {:user x}))
   :handle-ok :user)
 
-(defresource r-user-update [id data]
+(defresource r-user-create [data]
+  ;;; FIXME should check for existing user
   :allowed-methods [:post]
   :available-media-types ["application/json"]
-  :handle-ok (user-update id data))
+  :malformed? (fn [_] (new-user-valid? data))
+  :post! (fn [_] (user-create! data)))
+
+(defresource r-user-update [id data]
+  :allowed-methods [:put]
+  :available-media-types ["application/json"]
+  :exists? (fn [_] (user-get id))
+  :put! (fn [_] (user-update id data))
+  :new? (constantly false)
+  :respond-with-entity? (constantly false))
+
+(defresource r-visit-beacon [id visit]
+  :allowed-methods [:post]
+  :available-media-types ["application/json"]
+  :exists? (fn [ctx] (user-get id))
+  :malformed? (fn [ctx] (visit-valid? visit))
+  :handle-ok (fn [_] (handle-visit id visit)))
+
 
 (defroutes app
   (context "/api" []
-           (POST "/users/:id"
+           (POST "/users"
+                 {body :body}
+                 (r-user-create (keywordize-keys body)))
+           (PUT "/users/:id"
                  {{id :id} :params body :body}
                  (r-user-update id (keywordize-keys body)))
            (GET "/users/:id"
@@ -68,6 +110,7 @@
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
     (jetty/run-jetty (-> #'app
+                         (wrap-trace :header :ui)
                          wrap-json-response
                          wrap-json-body
                          (wrap-defaults (assoc site-defaults :security {:anti-forgery false}))
